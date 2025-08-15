a83f62a7a172dd8198e6250efa30fa79b84e06cd
David Patiashvili <870169+davaxi@users.noreply.github.com>
2025-08-15 14:34:17 +0200

    Engine: Create command to apply elements
    Changelog: added

--- src/po_sync/cli.py
+++ src/po_sync/cli.py
@@ -7,14 +7,15 @@ def main():
         add_help=True
     )
     parser.add_argument('file_path', help='Path to .po file with corrections')
-    parser.add_argument('-d', '--folder-path', help='Path to base project\'s folder', nargs='?', default='.')
+    parser.add_argument('-d', '--base-project', help='Path to base project\'s folder', nargs='?', default='.')
     parser.add_argument('-t', '--dry-run', help='Dry run', action='store_true')
     parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
     parser.add_argument('-y', '--yes', help='Do not ask for confirmation', action='store_true')
+    parser.add_argument('-c', '--clear', help='Clear msgstr after processing', action='store_true')
 
     args = parser.parse_args()
 
-    engine = PoSync(args.file_path, args.folder_path, args.dry_run, args.verbose, args.yes)
+    engine = PoSync(args.file_path, args.base_project, args.dry_run, args.verbose, args.yes, args.clear)
     engine.run()
 
 if __name__ == "__main__":
